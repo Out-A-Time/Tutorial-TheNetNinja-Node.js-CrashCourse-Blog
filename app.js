8d8f4fb2d42a95b4b14829a0897635f360620b1b
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-09-25 21:24:15 +0100

    MVC done and other refactoring. That was fun!

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const PORT = 3000;
 
 const mongoUSER = process.env.mongoUSER;
 const mongoPASSWORD = process.env.mongoPASSWORD;
-//Importing mongoDB model
-const Blog = require("./models/blog.js");
+
 const { render } = require("ejs");
+const blogRoutes = require("./routes/blogRoutes");
 
 //express app
 const app = express();
@@ -100,57 +100,8 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All blogs", allBlogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  console.log(req.body);
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      return res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+//Blog Routes
+app.use("/blogs", blogRoutes);
 
 //404 Page - default case
 //We use method .use() to create middleware thats fire middleware functions in Express
